97e54447845cccf54c7258a5abc44d5bfcad29c8
merlytalma <merlytalma@gmail.com>
2024-10-04 12:56:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched  # Correct import for both col and when_matched
 
 # Write directly to the app
